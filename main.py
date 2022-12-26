2a97bfe3f99b7df55334d00c0949b63fe29c464b
RickLin <83101411+RickLin616@users.noreply.github.com>
2022-12-26 22:29:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,11 +9,17 @@ from video_extraction import *
 st.title("Video Extraction Tool")
 video = st.file_uploader("Upload your mp4 video.", ["mp4",])
 st.video(video)
+'''
 if video:
     tpath = tempfile.NamedTemporaryFile(delete=False)
     tpath.write(video.read())
     st.image(extract_bg(tpath.name),use_column_width=True)
-
+'''
+if video is not None: # run only when user uploads video
+    vid = video.name
+    with open(vid, mode='wb') as f:
+        f.write(video.read()) 
+    st.image(extract_bg(tpath.name),use_column_width=True)
 
 
 
