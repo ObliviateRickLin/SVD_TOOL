5162506a6313881befc25a22ad1d8205e1cdcee1
RickLin <83101411+RickLin616@users.noreply.github.com>
2022-12-26 22:55:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,17 +12,18 @@ st.video(video)
 
 
 with st.form("File uploaded and video extraction"):
-    algorithm = st.selectbox(options=["SCIPY SVD","POWER ITERATION"])
+    algorithm = st.selectbox(label = "Algorithm", options=["SCIPY SVD","POWER ITERATION"])
     interval = st.slider("Interval",min_value = 20, max_value= 40)
     if video is not None: # run only when user uploads video
         vid = video.name
         with open(vid, mode='wb') as f:
             f.write(video.read()) 
     if st.form_submit_button("Extraction"):
-        if algorithm == "SCIPY SVD":
-            st.image(extract_bg(vid),use_column_width=True)
-        elif algorithm == "POWER ITERATION":
-            st.image(extract_bg_(vid),use_column_width=True)
+        with st.spinner("Waite for a while. The algorithm is extracting the background."):
+            if algorithm == "SCIPY SVD":
+                st.image(extract_bg(vid),use_column_width=True)
+            elif algorithm == "POWER ITERATION":
+                st.image(extract_bg_(vid),use_column_width=True)
 
 
 
